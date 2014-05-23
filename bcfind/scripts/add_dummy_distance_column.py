44ce3e70279a82b35ae79023e015e72a9c86eb10
ZioCorto <roberto.cortini@gmail.com>
2014-05-23 21:34:23 +0200

    Uses argparse

--- bcfind/scripts/add_dummy_distance_column.py
+++ bcfind/scripts/add_dummy_distance_column.py
@@ -6,19 +6,16 @@ import pandas as pd
 import numpy as np
 import sys
 import parameters
+import argparse
 
 
-def usage():
-    print "python " + sys.argv[0] + " infile outfile"
-    print "infile: markers file without the distance column"
-    print "outfile: markers file with a column named distance with all zero values added"
+parser = argparse.ArgumentParser(description="add a distance column, filled with zeros, to an existing markers file")
+parser.add_argument("infile", help="input markers file without the distance column")
+parser.add_argument("outfile", help="output markers file with a column named distance with all zero values added")
+args = parser.parse_args()
 
-if len(sys.argv) != 3:
-    usage()
-    sys.exit(1)
-
-infile = sys.argv[1]
-outfile = sys.argv[2]
+infile = args.infile
+outfile = args.outfile
 
 df = pd.read_csv(infile)
 df[parameters.distance_col] = np.zeros((df.shape[0], ))
