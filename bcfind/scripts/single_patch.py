aea1760606d4f4c362ebb80f3ef118b1be086123
ZioCorto <roberto.cortini@gmail.com>
2014-05-23 21:38:51 +0200

    Uses argparse, more debug

--- bcfind/scripts/single_patch.py
+++ bcfind/scripts/single_patch.py
@@ -14,42 +14,43 @@ from SurfaceCleaner import SurfaceCleaner
 import pandas as pd
 import utils
 import parameters
+import argparse
 
 
-def usage():
-    print "python " + sys.argv[0] + " data_file outdir max_distance n_neighbors seeds_folder sigma debug"
-    print "data_file: path to the markers file"
-    print "outdir: where to save patches markers files"
-    print "max_distance: maximum geodesic radius for creating patches with uniform cost search"
-    print "n_neighbors: nearest neighbors used to build the nearest neighbors graph of the data"
-    print "seeds_folder: path to a folder containing seeds to start uniform cost search"
-    print "sigma: sigma parameter of the gaussian kernel used in lowess regression"
-    print "debug: decides if the script saves csv files for CloudCompare"
+parser = argparse.ArgumentParser(description="compute reconstruction distances for a set of patches")
+parser.add_argument("data_file", help="path to the markers file")
+parser.add_argument("outdir", help="where to save patches markers files")
+parser.add_argument("max_distance", help="maximum geodesic radius for creating patches with uniform cost search", type=float)
+parser.add_argument("n_neighbors", help="nearest neighbors used to build the nearest neighbors graph of the data", type=int)
+parser.add_argument("seeds_folder", help="path to a folder containing seeds to start uniform cost search")
+parser.add_argument("sigma", help="sigma parameter of the gaussian kernel used in lowess regression", type=float)
+parser.add_argument("--debug", help="saves csv debug files", action="store_true")
+args = parser.parse_args()
 
-if len(sys.argv) != 8:
-    usage()
-    sys.exit(1)
-
-data_file = sys.argv[1]
-outdir = utils.add_trailing_slash(sys.argv[2])
+data_file = args.data_file
+outdir = utils.add_trailing_slash(args.outdir)
 
 utils.make_dir(outdir)
 
 data_frame = pd.read_csv(data_file)
 
-max_distance = float(sys.argv[3])
-n_neighbors = int(sys.argv[4])
-seeds_folder = utils.add_trailing_slash(sys.argv[5])
-sigma = float(sys.argv[6])
-debug = int(sys.argv[7])
+max_distance = args.max_distance
+n_neighbors = args.n_neighbors
+seeds_folder = utils.add_trailing_slash(args.seeds_folder)
+sigma = args.sigma
+debug = args.debug
 
 if debug:
     outdir_embeddings = outdir + 'embeddings/'
     outdir_reconstructions = outdir + 'reconstructions/'
     outdir_csvs = outdir + 'csv_patches/'
+    outdir_single_points = outdir + 'single_points/'
+    outdir_faulty = outdir + 'faulty/'
     utils.make_dir(outdir_embeddings)
     utils.make_dir(outdir_reconstructions)
     utils.make_dir(outdir_csvs)
+    utils.make_dir(outdir_single_points)
+    utils.make_dir(outdir_faulty)
 
 seeds = utils.get_filenames(seeds_folder)
 
@@ -63,12 +64,23 @@ for seed in seeds:
     if len(patch) == 1:
         print "There is one point in patch from seed " + seed + " with geodesic radius " + repr(max_distance)
         print "Most likely a false positive, skipping..."
+        if debug:
+            print "Saving patch with one point for debug purposes..."
+            single_frame_patch = data_frame[data_frame.index.isin(patch)]
+            single_frame_patch.to_csv(outdir_single_points + seed + '.csv', index=False)
+        continue
+    elif len(patch) == 2:
+        print "There are two points in patch from seed " + seed + " with geodesic radius " + repr(max_distance)
+        print "Most likely two false positives, skipping..."
+        if debug:
+            print "Saving patch with two points for debug purposes..."
+            single_frame_patch = data_frame[data_frame.index.isin(patch)]
+            single_frame_patch.to_csv(outdir_single_points + seed + '.csv', index=False)
         continue
 
     data_frame_patch = data_frame[data_frame.index.isin(patch)]
 
     X_patch = data_frame_patch.as_matrix([parameters.x_col, parameters.y_col, parameters.z_col])
-    name_patch = data_frame_patch[parameters.name_col]
 
     n_neighbors_patch = graph_utils.compute_minimum_nearest_neighbors(X_patch)
 
@@ -78,6 +90,9 @@ for seed in seeds:
     except ValueError:
         print "Processing seed " + seed + "..."
         print "Got a strange ValueError due to sparse representation, skipping the patch..."
+        if debug:
+            print "Saving faulty patch for debug purposes..."
+            data_frame_patch.to_csv(outdir_faulty + seed + '.csv', index=False)
         continue
 
     metric = EuclideanMetric()
