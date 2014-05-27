6a88b4db170712346d0b7d3201e0be9a414cb103
Roberto Cortini <roberto@armonia.dinfo.unifi.it>
2014-05-27 16:16:04 +0200

    argparse for documentation

--- bcfind/scripts/add_dummy_distance_column.py
+++ bcfind/scripts/add_dummy_distance_column.py
@@ -5,18 +5,30 @@ Invoke this script to add a distance column, filled with zeros, to an existing m
 import pandas as pd
 import numpy as np
 import sys
-import parameters
+import manifold.parameters
 import argparse
 
 
-parser = argparse.ArgumentParser(description="add a distance column, filled with zeros, to an existing markers file")
-parser.add_argument("infile", help="input markers file without the distance column")
-parser.add_argument("outfile", help="output markers file with a column named distance with all zero values added")
-args = parser.parse_args()
+def get_parser():
+    parser = argparse.ArgumentParser(description=__doc__,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    
+    parser.add_argument("infile", help="input markers file without the distance column")
+    parser.add_argument("outfile", help="output markers file with a column named distance with all zero values added")
 
-infile = args.infile
-outfile = args.outfile
+    return parser
 
-df = pd.read_csv(infile)
-df[parameters.distance_col] = np.zeros((df.shape[0], ))
-df.to_csv(outfile, index=False)
+
+def main(args):
+    infile = args.infile
+    outfile = args.outfile
+    
+    df = pd.read_csv(infile)
+    df[parameters.distance_col] = np.zeros((df.shape[0], ))
+    df.to_csv(outfile, index=False)
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    main(args)
