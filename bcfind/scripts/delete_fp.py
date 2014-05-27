9bda54e96b556b31c41686e3ef064c04faaff6c1
Roberto Cortini <roberto@armonia.dinfo.unifi.it>
2014-05-27 16:16:37 +0200

    argparse for documentation

--- bcfind/scripts/delete_fp.py
+++ bcfind/scripts/delete_fp.py
@@ -4,23 +4,35 @@ Invoke this script to delete all points above the given threshold in a markers f
 
 import sys
 import pandas as pd
-import utils
-import parameters
+import manifold.utils
+import manifold.parameters
 import argparse
 
 
-parser = argparse.ArgumentParser(description="delete all points above the given threshold in a markers file")
-parser.add_argument("data_file", help="path to the markers file with saved reconstruction distances")
-parser.add_argument("outdir", help="where to save filtered markers file")
-parser.add_argument("threshold", type=float, help="float value used to delete points with reconstruction distances > threshold")
-args = parser.parse_args()
+def get_parser():
+    parser = argparse.ArgumentParser(description=__doc__,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    
+    parser.add_argument("data_file", help="path to the markers file with saved reconstruction distances")
+    parser.add_argument("outdir", help="where to save filtered markers file")
+    parser.add_argument("threshold", type=float, help="float value used to delete points with reconstruction distances > threshold")
 
-data_file = args.data_file
-outdir = utils.add_trailing_slash(args.outdir)
-threshold = args.threshold
+    return parser
 
-utils.make_dir(outdir)
 
-data_frame = pd.read_csv(data_file)
-filtered_data_frame = data_frame[data_frame[parameters.distance_col] <= threshold].drop(parameters.distance_col, axis=1)
-filtered_data_frame.to_csv(outdir + 'threshold_' + repr(threshold) + '.marker', index=False)
+def main(args):
+    data_file = args.data_file
+    outdir = utils.add_trailing_slash(args.outdir)
+    hreshold = args.threshold
+    
+    utils.make_dir(outdir)
+    
+    data_frame = pd.read_csv(data_file)
+    filtered_data_frame = data_frame[data_frame[parameters.distance_col] <= threshold].drop(parameters.distance_col, axis=1)
+    filtered_data_frame.to_csv(outdir + 'threshold_' + repr(threshold) + '.marker', index=False)
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    main(args)
