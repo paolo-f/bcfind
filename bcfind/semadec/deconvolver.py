412680e041e53b75f2ec764226e49cdfd8639b59
paolo-f <p-f@dsi.unifi.it>
2014-08-19 19:22:52 +0200

    multiview deconvolver

--- bcfind/semadec/deconvolver.py
+++ bcfind/semadec/deconvolver.py
@@ -6,6 +6,7 @@ import theano
 
 from pylearn2.models.autoencoder import *
 from pylearn2.models.mlp import MLP
+import timeit
 
 
 class VPrint(object):
@@ -68,11 +69,11 @@ class PredictorFromPylearn2MLP(Predictor):
         self.Xhat_sv = model.fprop(self.X_sv)
         self.reconstruction_function = theano.function([self.X_sv],[self.Xhat_sv])
 
-    def predict(self,X,batch_size=None):
+    def predict(self,X,batch_size=None, n_views=1):
         if batch_size is None:
             batch_size = X.shape[0]
         n_batches = X.shape[0]/batch_size
-        Xhat = np.zeros(X.shape)
+        Xhat = np.zeros((X.shape[0],X.shape[1]/n_views))
         for i in xrange(n_batches):
             xx = self.reconstruction_function(X[i*batch_size:(i+1)*batch_size,:])
             Xhat[i*batch_size:(i+1)*batch_size] = xx[0]
@@ -142,6 +143,98 @@ def filter_volume(np_tensor_3d, Xmean, Xstd,
         extramargin:sc[0]-extramargin,
         extramargin:sc[1]-extramargin,
         extramargin:sc[2]-extramargin]
+    print_percentiles(reconstruction, 'Reconstruction')
+    reconstruction = normalize(reconstruction, normalizer)
+    print_percentiles(reconstruction, 'Normalized reconstruction')
+    # # smooth aliasing due to speedup
+    reconstruction = filters.gaussian_filter(reconstruction,sigma=speedup/3.0, mode='constant', cval=0.0)
+    print_percentiles(reconstruction, 'Normalized reconstruction')
+    # reconstruction = filters.median_filter(reconstruction,size=3, mode='constant', cval=0.0)
+    # print_percentiles(reconstruction, 'Medianized reconstruction')
+    reconstruction = np.array(reconstruction*255.0, dtype=np.uint8)
+    print_percentiles(reconstruction, 'reconstruction 8 bit')
+    return reconstruction
+
+
+def filter_multiview_volume(Xmean, Xstd, np_tensor_3d_first_view, np_tensor_3d_second_view,
+                            extramargin=None, model=None, speedup=None, sparsity_measure_type=None):
+
+    np_tensor_3d_first_view = np_tensor_3d_first_view / 255.0
+    np_tensor_3d_second_view = np_tensor_3d_second_view / 255.0
+
+    print('np_tensor_3d_first_view.shape=', np_tensor_3d_first_view.shape)
+    print('np_tensor_3d_second_view.shape=', np_tensor_3d_second_view.shape)
+
+    predictor = make_predictor(model)
+    sc = np_tensor_3d_first_view.shape
+    reconstruction = np.zeros(sc)
+    patchlen = (1+2*extramargin)**3
+    rangez = range(extramargin, sc[0]-extramargin, speedup)
+    rangey = range(extramargin, sc[1]-extramargin, speedup)
+    rangex = range(extramargin, sc[2]-extramargin, speedup)
+
+    n_points = len(rangex)*len(rangey)
+
+    normalizer = np.zeros(sc)
+    print('Patch size: %dx%dx%d (%d)' % (1+2*extramargin, 1+2*extramargin, 1+2*extramargin, (1+2*extramargin)**3))
+    print('Will subsample jumping by',speedup,'voxels')
+    pbar_prediction = ProgressBar(widgets=['Pre-processing %d slices (%d patches): ' % (len(rangez),len(rangex)*len(rangey)*len(rangez)),
+                                  Percentage(), ' ', AdaptiveETA()])
+    bar_reconstruction = ProgressBar(widgets=['Post-processing %d slices (%d patches): ' % (len(rangez),len(rangex)*len(rangey)*len(rangez)),
+                                     Percentage(), ' ', AdaptiveETA()])
+
+    ndone = 0
+    data = np.zeros((len(rangez) * n_points, patchlen*2))
+    iter_z = 0
+    for z0 in pbar_prediction(rangez):
+        i = 0
+        for y0 in rangey:
+            for x0 in rangex:
+                patch_first_view = np_tensor_3d_first_view[z0-extramargin:z0+extramargin+1,
+                                                           y0-extramargin:y0+extramargin+1,
+                                                           x0-extramargin:x0+extramargin+1]
+                patch_second_view = np_tensor_3d_second_view[z0-extramargin:z0+extramargin+1,
+                                                             y0-extramargin:y0+extramargin+1,
+                                                             x0-extramargin:x0+extramargin+1]
+
+                data[iter_z * n_points + i][0:patchlen] = np.reshape(patch_first_view, (patchlen,))
+                data[iter_z * n_points + i][patchlen:2*patchlen] = np.reshape(patch_second_view, (patchlen,))
+                data[iter_z * n_points + i] = (data[iter_z * n_points + i] - Xmean) / Xstd
+                i += 1
+        iter_z += 1
+    print('Predicting ',data.shape[0],' patches ...')
+    pred_start = timeit.default_timer()
+    data = predictor.predict(data,n_views=2)
+    pred_stop = timeit.default_timer()
+    print('Prediction done in %s seconds' % (pred_stop - pred_start))
+    iter_z = 0
+    for z0 in bar_reconstruction(rangez):
+        i = 0
+        for y0 in rangey:
+            for x0 in rangex:
+                reconstructed_patch = np.reshape(data[iter_z * n_points + i], (1+2*extramargin,1+2*extramargin,1+2*extramargin))
+                # print_percentiles(reconstructed_patch, 'Reconstructed patch at (%d,%d,%d)'%(z0,y0,x0))
+                # reconstructed_patch = np.reshape(data[i], (1+2*extramargin,1+2*extramargin,1+2*extramargin))
+                reconstruction[z0-extramargin:z0+extramargin+1,
+                               y0-extramargin:y0+extramargin+1,
+                               x0-extramargin:x0+extramargin+1] += reconstructed_patch
+                normalizer[z0-extramargin:z0+extramargin+1,
+                           y0-extramargin:y0+extramargin+1,
+                           x0-extramargin:x0+extramargin+1] += 1
+                i += 1
+                ndone += 1
+        iter_z += 1
+    vprint('Reconstructed and assembled',ndone,'patches, now clipping..')
+
+    reconstruction = reconstruction[
+        extramargin:sc[0]-extramargin,
+        extramargin:sc[1]-extramargin,
+        extramargin:sc[2]-extramargin]
+    normalizer = normalizer[
+        extramargin:sc[0]-extramargin,
+        extramargin:sc[1]-extramargin,
+        extramargin:sc[2]-extramargin]
+
     print_percentiles(reconstruction, 'Reconstruction')
     reconstruction = normalize(reconstruction, normalizer)
     print_percentiles(reconstruction, 'Normalized reconstruction')
