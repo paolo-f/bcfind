b60cbacd8c386df863e3257f1174c72c60fd6eff
Marco Paciscopi <marco.paciscopi@stud.unifi.it>
2015-04-22 14:16:47 +0200

    added local_entropy.pyx entry to the setup config file

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
 import numpy
 
 setup(
-    ext_modules = cythonize("bcfind/fast_threshold.pyx"),
+    ext_modules = cythonize(["bcfind/fast_threshold.pyx", "bcfind/local_entropy.pyx"]),
     include_dirs=[numpy.get_include()]
 )
+
 
 # python setup.py build_ext --inplace
