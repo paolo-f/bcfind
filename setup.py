89633a1e5c7709183ac2ebe73bed17b7fd69d209
Marco Paciscopi <marco.paciscopi@stud.unifi.it>
2015-04-29 01:01:37 +0200

    added the cython source file bcfind/extract_patch.pyx to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
+from distutils.extension import Extension
 
 import numpy
 
 setup(
-    ext_modules = cythonize(["bcfind/fast_threshold.pyx", "bcfind/local_entropy.pyx"]),
-    include_dirs=[numpy.get_include()]
+    ext_modules = cythonize(["bcfind/fast_threshold.pyx", "bcfind/local_entropy.pyx","bcfind/extract_patch.pyx"]),
+    include_dirs=[numpy.get_include()],
+
 )
 
 
