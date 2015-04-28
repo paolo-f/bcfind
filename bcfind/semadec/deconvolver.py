a44371e8590685a6a73f250dfeb6e42270803514
Marco Paciscopi <marco.paciscopi@stud.unifi.it>
2015-04-29 01:13:16 +0200

    added the interface to external compiled code that preprocesses the raw data and reconstructs the deconvolved tensor

--- bcfind/semadec/deconvolver.py
+++ bcfind/semadec/deconvolver.py
@@ -8,7 +8,7 @@ from pylearn2.models.autoencoder import *
 from pylearn2.models.mlp import MLP
 import tables
 import timeit
-
+from bcfind import extract_patch
 
 class VPrint(object):
     def __init__(self,verbose=True):
@@ -67,26 +67,16 @@ class PredictorFromPylearn2MLP(Predictor):
         self.model = model
         self.X_sv = model.get_input_space().make_batch_theano()
         self.Xhat_sv = model.fprop(self.X_sv)
-
-        if isinstance(self.X_sv, tuple):
-            self.reconstruction_function = theano.function(self.X_sv,[self.Xhat_sv])
-        else:
-            self.reconstruction_function = theano.function([self.X_sv],[self.Xhat_sv])
+        self.reconstruction_function = theano.function([self.X_sv],[self.Xhat_sv], allow_input_downcast=True)
 
 
     def predict(self,X,model,batch_size=None, n_views=1):
         if batch_size is None:
             batch_size = X.shape[0]
         n_batches = X.shape[0]/batch_size
-        Xhat = np.zeros((X.shape[0],X.shape[1]/n_views))
+        Xhat = np.zeros((X.shape[0],X.shape[1]/n_views), dtype=np.float32)
         for i in xrange(n_batches):
-
-            if isinstance(model.get_input_space(), VectorSpace):
-                xx = self.reconstruction_function(X[i*batch_size:(i+1)*batch_size,:])
-            elif isinstance(model.get_input_space(), CompositeSpace):
-                xx = self.reconstruction_function(X[i*batch_size:(i+1)*batch_size,0:X.shape[1]/n_views],X[i*batch_size:(i+1)*batch_size,X.shape[1]/n_views:X.shape[1]])
-            else:
-                raise ValueError("Can't support input space " + str(model.get_input_space().__class__))
+            xx = self.reconstruction_function(X[i*batch_size:(i+1)*batch_size,:])
             Xhat[i*batch_size:(i+1)*batch_size] = xx[0]
         return Xhat
 
@@ -100,84 +90,68 @@ def make_predictor(model):
         raise TypeError('Model has type', type(model))
 
 
-def filter_volume(np_tensor_3d, size_patch, trainfile,local_mean_std=False,
-                  extramargin=None, model=None, speedup=None):
+def filter_volume(np_tensor_3d, Xmean, Xstd,
+                  extramargin=None, model=None, speedup=None, do_cython=False):
 
-
-    np_tensor_3d = np_tensor_3d / 255.0
 
     predictor = make_predictor(model)
     sc = np_tensor_3d.shape
-    reconstruction = np.zeros(sc)
+    reconstruction = np.zeros(sc, dtype=np.float32)
+    reconstruction_2 = np.zeros(sc, dtype=np.float32)
     n_points = (sc[1]-2*extramargin)*(sc[2]-2*extramargin)
-    patchlen = (1+2*size_patch)**3
+    patchlen = (1+2*extramargin)**3
     rangez = range(extramargin, sc[0]-extramargin, speedup)
     rangey = range(extramargin, sc[1]-extramargin, speedup)
     rangex = range(extramargin, sc[2]-extramargin, speedup)
     n_points = len(rangex)*len(rangey)
 
-
-    normalizer = np.zeros(sc)
-    print('Patch size: %dx%dx%d (%d)' % (1+2*size_patch, 1+2*size_patch, 1+2*size_patch, (1+2*size_patch)**3))
+    normalizer = np.zeros(sc, dtype=np.float32)
+    normalizer_2 = np.zeros(sc, dtype=np.float32)
+    print('Patch size: %dx%dx%d (%d)' % (1+2*extramargin, 1+2*extramargin, 1+2*extramargin, (1+2*extramargin)**3))
     print('Will subsample jumping by',speedup,'voxels')
-    bar_prediction = ProgressBar(widgets=['Pre-processing %d slices (%d patches): ' % (len(rangez),len(rangex)*len(rangey)*len(rangez)),
-                                  Percentage(), ' ', AdaptiveETA()])
-    bar_reconstruction = ProgressBar(widgets=['Post-processing %d slices (%d patches): ' % (len(rangez),len(rangex)*len(rangey)*len(rangez)),
-                                     Percentage(), ' ', AdaptiveETA()])
+    pbar = ProgressBar(widgets=['Processing %d slices (%d patches): ' % (len(rangez),len(rangex)*len(rangey)*len(rangez)),
+                                Percentage(), ' ', ETA()])
 
-    data = np.zeros((len(rangez) * n_points, patchlen))
-    iter_z=0
-    for z0 in bar_prediction(rangez):
-        i = 0
-        for y0 in rangey:
-            for x0 in rangex:
-                patch = np_tensor_3d[z0-size_patch:z0+size_patch+1,
-                                     y0-size_patch:y0+size_patch+1,
-                                     x0-size_patch:x0+size_patch+1]
-                data[iter_z * n_points + i] = np.reshape(patch, (patchlen,))
-                i += 1
-        iter_z += 1
+    np_tensor_3d=(np_tensor_3d/255.).astype(np.float32)
 
+    if do_cython:
 
-    if local_mean_std:
-        print('Reading standardization data from substack')
-        Xmean = data.mean(axis=0)
-        Xstd = data.std(axis=0)
+        for z0 in pbar(rangez):
+
+            data= extract_patch.preprocess_2d(np_tensor_3d, z0, extramargin, extramargin, speedup, Xmean, Xstd)
+            pred_data = predictor.predict(data, model)
+            reconstruction,normalizer = extract_patch.postprocess_2d(np_tensor_3d, z0, pred_data, reconstruction, normalizer, extramargin, extramargin, speedup)
     else:
-        print('Reading standardization data from trainfile', trainfile)
-        h5 = tables.openFile(trainfile)
-        Xmean = h5.root.Xmean[:]
-        Xstd = h5.root.Xstd[:]
-        h5.close()
 
-    std_start = timeit.default_timer()
-    data = (data - Xmean) / Xstd
-    std_stop = timeit.default_timer()
-    print('Standardization done in %s seconds' % (std_stop - std_start))
-    pred_start = timeit.default_timer()
-    data = predictor.predict(data, model, n_views=1)
-    pred_stop = timeit.default_timer()
-    print('Prediction done in %s seconds' % (pred_stop - pred_start))
+        for z0 in pbar(rangez):
+            data = np.zeros((n_points, patchlen),  dtype=np.float32)
+            i = 0
+            for y0 in rangey:
+                for x0 in rangex:
+                    patch = np_tensor_3d[z0-extramargin:z0+extramargin+1,
+                                         y0-extramargin:y0+extramargin+1,
+                                         x0-extramargin:x0+extramargin+1]
+                    data[i] = np.reshape(patch, (patchlen,))
+                    data[i] = (data[i] - Xmean)/Xstd
+                    i += 1
+
+            pred_data = predictor.predict(data, model)
+
+            i = 0
+            for y0 in rangey:
+                for x0 in rangex:
+                    reconstructed_patch = np.reshape(pred_data[i], (1+2*extramargin,1+2*extramargin,1+2*extramargin))
+                    reconstruction[z0-extramargin:z0+extramargin+1,
+                                   y0-extramargin:y0+extramargin+1,
+                                   x0-extramargin:x0+extramargin+1] += reconstructed_patch
+                    normalizer[z0-extramargin:z0+extramargin+1,
+                               y0-extramargin:y0+extramargin+1,
+                               x0-extramargin:x0+extramargin+1] += 1
+                    i += 1
 
 
-    iter_z=0
-    ndone=0
-    for z0 in bar_reconstruction(rangez):
-        i = 0
-        for y0 in rangey:
-            for x0 in rangex:
-                reconstructed_patch = np.reshape(data[iter_z * n_points +i], (1+2*size_patch,1+2*size_patch,1+2*size_patch))
-                reconstruction[z0-size_patch:z0+size_patch+1,
-                                y0-size_patch:y0+size_patch+1,
-                                x0-size_patch:x0+size_patch+1] += reconstructed_patch
-                normalizer[z0-size_patch:z0+size_patch+1,
-                            y0-size_patch:y0+size_patch+1,
-                            x0-size_patch:x0+size_patch+1] += 1
-                i += 1
-                ndone += 1
-        iter_z += 1
 
-    vprint('Reconstructed and assembled',ndone,'patches, now clipping..')
+    vprint('Reconstructed and assembled',n_points*len(rangez),'patches, now clipping..')
     reconstruction = reconstruction[
         extramargin:sc[0]-extramargin,
         extramargin:sc[1]-extramargin,
@@ -186,16 +160,18 @@ def filter_volume(np_tensor_3d, size_patch, trainfile,local_mean_std=False,
         extramargin:sc[0]-extramargin,
         extramargin:sc[1]-extramargin,
         extramargin:sc[2]-extramargin]
-    print_percentiles(reconstruction, 'Reconstruction')
+    #print_percentiles(reconstruction, 'Reconstruction')
     reconstruction = normalize(reconstruction, normalizer)
-    print_percentiles(reconstruction, 'Normalized reconstruction')
+    #print_percentiles(reconstruction, 'Normalized reconstruction')
     # # smooth aliasing due to speedup
-    if speedup>1:
-        reconstruction = filters.gaussian_filter(reconstruction,sigma=speedup/3.0, mode='constant', cval=0.0)
-    print_percentiles(reconstruction, 'Normalized reconstruction')
+    reconstruction = filters.gaussian_filter(reconstruction,sigma=speedup/3.0, mode='constant', cval=0.0)
+    #print_percentiles(reconstruction, 'Normalized reconstruction')
+    # reconstruction = filters.median_filter(reconstruction,size=3, mode='constant', cval=0.0)
+    # print_percentiles(reconstruction, 'Medianized reconstruction')
     reconstruction = np.array(reconstruction*255.0, dtype=np.uint8)
-    print_percentiles(reconstruction, 'reconstruction 8 bit')
+    #print_percentiles(reconstruction, 'reconstruction 8 bit')
     return reconstruction
+
 
 
 def filter_multiview_volume(np_tensor_3d_first_view, np_tensor_3d_second_view, size_patch, trainfile, local_mean_std=False,
