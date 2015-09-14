f3e967568e346084701d27c0d3ccbfeaaac44e8f
Marco Paciscopi <mapaci89@gmail.com>
2015-09-14 14:52:10 +0200

    renamed fuse_stacks script to  fuse_tensors

---
+++ bcfind/scripts/fuse_tensors.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+"""
+Scripts that fuse 3D greyscale tensors in a 3D RGB tensor
+"""
+__author__ = 'paciscopi'
+
+import numpy as np
+import os
+from PIL import Image
+import glob
+import cv2
+import argparse
+
+
+def read_tensors(args):
+
+    suffix = ".tif"
+    convert_to_gray = True
+
+    filesRedChannel = sorted([args.redChannel_dir + '/' + f for f in os.listdir(args.redChannel_dir) if f[0] != '.' and f.endswith(suffix)])
+    img_z = np.asarray(Image.open(filesRedChannel[0]))
+    height_redChannel, width_redChannel = img_z.shape
+    depth_redChannel = len(filesRedChannel)
+    pixels_redChannel = np.empty(shape=(depth_redChannel, height_redChannel, width_redChannel), dtype=np.uint8)
+
+    for z, image_file in enumerate(filesRedChannel):
+        img_z = Image.open(image_file)
+        if convert_to_gray:
+            img_z = img_z.convert('L')
+        pixels_redChannel[z, :, :] = np.asarray(img_z)
+    print('...read the first tensor (%s slices)' %z)
+
+
+    filesGreenChannel = sorted([args.greenChannel_dir + '/' + f for f in os.listdir(args.greenChannel_dir) if f[0] != '.' and f.endswith(suffix)])
+    img_z = np.asarray(Image.open(filesGreenChannel[0]))
+    height_greenChannel, width_greenChannel = img_z.shape
+    depth_greenChannel = len(filesGreenChannel)
+    pixels_greenChannel = np.empty(shape=(depth_greenChannel, height_greenChannel, width_greenChannel), dtype=np.uint8)
+
+    for z, image_file in enumerate(filesGreenChannel):
+        img_z = Image.open(image_file)
+        if convert_to_gray:
+            img_z = img_z.convert('L')
+        pixels_greenChannel[z, :, :] = np.asarray(img_z)
+    print('...read the second tensor (%s slices)' %z)
+
+
+    if not args.blueChannel_dir:
+        pixels_blueChannel = np.zeros((depth_redChannel, height_redChannel, width_redChannel), dtype=np.uint8)
+    else:
+        filesBlueChannel = sorted([args.blueChannel_dir + '/' + f for f in os.listdir(args.blueChannel_dir) if f[0] != '.' and f.endswith(suffix)])
+        img_z = np.asarray(Image.open(filesBlueChannel[0]))
+        height_blueChannel, width_blueChannel = img_z.shape
+        depth_blueChannel = len(filesBlueChannel)
+        pixels_blueChannel = np.empty(shape=(depth_blueChannel, height_blueChannel, width_blueChannel), dtype=np.uint8)
+
+        for z, image_file in enumerate(filesBlueChannel):
+            img_z = Image.open(image_file)
+            if convert_to_gray:
+                img_z = img_z.convert('L')
+            pixels_blueChannel[z, :, :] = np.asarray(img_z)
+        print('...read the third tensor (%s slices)' %z)
+
+    return pixels_redChannel,pixels_greenChannel,pixels_blueChannel
+
+
+def fuse_tensors(args,pixels_redChannel,pixels_greenChannel,pixels_blueChannel):
+
+    if not os.path.exists(args.outputdir):
+        os.makedirs(args.outputdir)
+    else:
+        files = glob.glob(args.outputdir + '/*')
+        for f in files:
+            os.remove(f)
+    import uuid
+    num_slices=pixels_redChannel.shape[0]
+    for z in xrange(0, num_slices, 1):
+        pixels_merged = cv2.merge((pixels_redChannel[z], pixels_greenChannel[z], pixels_blueChannel[z]))
+        im = Image.fromarray(pixels_merged)
+        #im.save(args.outputdir + '/slice_' + str(z).zfill(4) + ".tif", 'TIFF')
+        tempname = '/tmp/'+str(uuid.uuid4())+'.tif'
+        im.save(tempname)
+        destname = args.outputdir+'/slice_'+str(z).zfill(4)+'.tif'
+        os.system('tiffcp -clzw:2 ' + tempname + ' ' + destname)
+        os.remove(tempname)
+    print('...fusion computed (%s slices) ' %z)
+    print('fused stack saved in ', args.outputdir)
+
+
+def main(args):
+    pixels_redChannel, pixels_greenChannel,pixels_blueChannel=read_tensors(args)
+    fuse_tensors(args,pixels_redChannel,pixels_greenChannel,pixels_blueChannel)
+
+def get_parser():
+    parser = argparse.ArgumentParser(description=__doc__,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('outputdir', metavar='outputdir', type=str,
+                        help='output folder where the fused 3D tensor will be saved')
+    parser.add_argument('redChannel_dir', metavar='redChannel_dir', type=str,
+                        help='path of the 3D red channel tensor')
+    parser.add_argument('greenChannel_dir', metavar='greenChannel_dir', type=str,
+                        help='path of the 3D green channel tensor')
+    parser.add_argument('--blueChannel_dir', metavar='blueChannel_dir', dest='blueChannel_dir', action='store', type=str,
+                        help='path of the 3D blue channel tensor')
+    return parser
+
+
+
+if __name__ == '__main__':
+
+    parser = get_parser()
+    args = parser.parse_args()
+    main(args)
