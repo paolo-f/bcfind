5d6a70c2ee4432a35bbb827e4ffad1822c7d683f
Marco Paciscopi <mapaci89@gmail.com>
2015-10-10 03:32:21 +0200

    added the matching algorithm based on ICP

--- bcfind/markers.py
+++ bcfind/markers.py
@@ -1,12 +1,15 @@
 import math
 import networkx
-
+from scipy.spatial.distance import cdist 
+import numpy as np
+from bcfind.volume import Center
+from bcfind.scripts.rotate_stacks import rigidMotionEstimation
 
 def distance((x1,y1,z1),(x2,y2,z2)):
     return math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
 
 
-def match_markers(C1,C2, max_distance):
+def match_markers(C1,C2, max_distance,verbose=False):
     """Match true and predicted markers using max-weight bipartite matching
 
     Parameters
@@ -42,8 +45,88 @@ def match_markers(C1,C2, max_distance):
             if d < max_distance:
                 w = 1.0/max(0.001,d)
                 G.add_edge(ni,nj,weight=w)
-    print("Solving max weight matching (%d nodes, %d edges)" % (len(G.nodes()), len(G.edges())))
+    if verbose:
+	print("Solving max weight matching (%d nodes, %d edges)" % (len(G.nodes()), len(G.edges())))
     mate = networkx.algorithms.matching.max_weight_matching(G,maxcardinality=False)
     return G,mate,node2center
+
+
+def compute_matches(c1,c2,max_distance):
+    dist_matrix = cdist(c1, c2, 'euclidean') 
+    range_col=np.arange(len(c2),dtype=int)
+    range_row=np.arange(len(c1),dtype=int)
+    good1=[]
+    good2=[]
+    distances=[]
+    end_match=False
+    j=min(len(c1),len(c2))
+    while not end_match and j>0:
+        min_dist=np.amin(dist_matrix)
+        if min_dist < max_distance:
+            ind_row=int(np.where(dist_matrix==min_dist)[0][0])
+            ind_col=int(np.where(dist_matrix==min_dist)[1][0])
+            dist_matrix[ind_row,:]=max_distance
+            dist_matrix[:,ind_col]=max_distance
+            good1.append(ind_row)
+            good2.append(ind_col)
+	    distances.append(min_dist)
+        else:
+            end_match=True
+        j-=1
+
+    return good1,good2,distances 
+    
+
+def match_markers_with_icp(C1,C2, max_distance,num_iterations = 100, eps=1e-8, verbose=False):
+
+    c1 = np.array([[c.x, c.y, c.z] for c in C1])
+    c2 = np.array([[c.x, c.y, c.z] for c in C2])
+
+    if c1.ndim == 1:
+        c1=np.expand_dims(c1, axis=0)
+    if c2.ndim == 1:
+        c2=np.expand_dims(c2, axis=0)
+
+    good1,good2,distances = compute_matches(c1,c2,max_distance)
+    c1_good = c1[good1]
+    c2_good = c2[good2]
+
+    R=np.eye(3)
+    t=np.zeros(3)
+    hom_matrix=np.vstack((np.hstack((R,np.expand_dims(t, axis=0).T)),np.array([0.,0.,0.,1.])))
+    hom_matrix_tot=hom_matrix
+    for i in xrange(num_iterations):
+
+        hom_previous_matrix=hom_matrix
+        
+        weights=1./(np.array(distances)+1) 
+        if len(c2_good) >3.:
+            R,t,_,_,_ =rigidMotionEstimation(c2_good, c1_good, weights)
+        else:
+            break
+        c2=np.dot(R,c2.T).T+t
+        hom_matrix=np.vstack((np.hstack((R,np.expand_dims(t, axis=0).T)),np.array([0.,0.,0.,1.])))
+        hom_matrix_tot=np.dot(hom_matrix_tot,hom_matrix)
+        incr_froeb_diff=np.linalg.norm(hom_matrix - hom_previous_matrix)
+        good1,good2,distances = compute_matches(c1,c2,max_distance)
+        c1_good = c1[good1]
+        c2_good = c2[good2] 
+        if verbose:
+            print('iter_icp:%d R:%s t:%s froeb_norm:%f'%(i,R,t,incr_froeb_diff))
+        if incr_froeb_diff <= eps:
+            break
+
+    R_tot = hom_matrix_tot[0:3,0:3]
+    t_tot = hom_matrix_tot[0:3,3]
+    C2_t_final=[]
+    for c,C in zip(c2,C2):
+        c_t=Center(c[0],c[1],c[2])
+        c_t.name=C.name
+        c_t.hue=C.hue
+        C2_t_final.append(c_t)
+
+    return C2_t_final,good1,good2,R_tot,t_tot
+
+
 
 
